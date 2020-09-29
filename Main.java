868ad45994f4a49019d840f354f810ed532ad18c
IzzyEhnes <izzyehnes@gmail.com>
2020-09-29 11:37:46 -0700

    Feature: Add Doubly class

--- Main.java
+++ Main.java
@@ -69,11 +69,87 @@ class Node
 
 
 
+class Doubly
+{
+    private Node head = null;
+    private Node tail = null;
+
+
+
+    public Doubly()
+    {
+        head.setPrevious(null);
+        tail.setNext(null);
+    }
+
+
+
+    void add(String inValue)
+    {
+        Node newNode = new Node(inValue);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+        {
+            // If newNode comes before head
+            if (newNode.getValue().compareTo(head.getValue()) < 0)
+            {
+                newNode.setNext(head);
+                newNode.setPrevious(null);
+                head.setPrevious(newNode);
+                head = newNode;
+            }
+
+            // If newNode comes after tail
+            else if (newNode.getValue().compareTo(tail.getValue()) > 0)
+            {
+                newNode.setNext(null);
+                newNode.setPrevious(tail);
+                tail.setNext(newNode);
+                tail = newNode;
+            }
+
+            // If newNode is somewhere in the middle
+            else
+            {
+                for (Node current = head; current != null; current = current.getNext())
+                {
+                    // If the name is already present in the list, ignore it
+                    if (current.getValue().equals(newNode.getValue()))
+                    {
+                        break;
+                    }
+
+                    if (current.getValue().compareTo(newNode.getValue()) > 0 &&
+                            current.getNext().getValue().compareTo(newNode.getValue()) < 0)
+                    {
+                        Node temp = current.getNext();
+                        current.setNext(newNode);
+                        newNode.setPrevious(current);
+                        newNode.setNext(temp);
+                        temp.setPrevious(newNode);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-	// write your code here
+        
     }
 }
